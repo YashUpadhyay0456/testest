a27d4a756500d76110752ba0e507ff0c0009e657
Yash Upadhyay <91269999+YashUpadhyay0456@users.noreply.github.com>
2025-05-24 19:49:25 +0530

    Feature learning branch (#2)
    * learning first commit
    
    * second commit

--- Main.java
+++ Main.java
@@ -13,6 +13,8 @@ public class Main {
       m = scanner.nextInt(); 
       l = scanner.nextInt();
       r = scanner.nextInt();
+      System.out.println("this is for learning git git hub version controlling");
+      System.out.println("this is for learning git git hub version controlling");
       if(n == m){
         System.out.println(l + " " + r);
         continue;
